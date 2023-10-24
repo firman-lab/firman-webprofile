6368a9f0db8337e05cfb7736d4a281d16448bc27
firman-lab <fhidayat400@gmail.com>
2023-10-24 11:19:12 +0700

    update

--- components/Header/index.js
+++ components/Header/index.js
@@ -28,7 +28,6 @@ export default function Header() {
                                     autoStart: true,
                                     loop: true,
                                     deleteSpeed: 40,
-                                    cursor: '|'
 
                                 }}
                             />
