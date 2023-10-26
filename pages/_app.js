a169cbfcadaa80c2f6a364284145c56e0775b7a5
firman-lab <fhidayat400@gmail.com>
2023-10-26 12:42:57 +0700

    update GA

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,14 @@ export default function App({ Component, pageProps }) {
 
     return (
         <>
-            <Script strategy='afterInteractive' async src={`https://www.googletagmanager.com/gtag/js?id=${ID}`}></Script>
+            <Script strategy='afterInteractive' async src={'https://www.googletagmanager.com/gtag/js?id=G-MS1TSQE338'}></Script>
             <Script id="google-analytics" strategy='afterInteractive'>
                 {`
                window.dataLayer = window.dataLayer || [];
                function gtag(){dataLayer.push(arguments);}
                gtag('js', new Date());
              
-               gtag('config', ${ID});
+               gtag('config', G-MS1TSQE338);
             `}
             </Script>
             <Component {...pageProps} />
