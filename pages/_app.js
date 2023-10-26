8d4da0f316987d2c04d37a762d5a2f5248594bfe
firman-lab <fhidayat400@gmail.com>
2023-10-26 12:34:27 +0700

    add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
+import Script from 'next/script';
 import '../styles/globals.css';
+const ID = process.env.GA_MEASUREMENT_ID;
 
 export default function App({ Component, pageProps }) {
 
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Script strategy='afterInteractive' async src={`https://www.googletagmanager.com/gtag/js?id=${ID}`}></Script>
+            <Script id="google-analytics" strategy='afterInteractive'>
+                {`
+               window.dataLayer = window.dataLayer || [];
+               function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+             
+               gtag('config', ${ID});
+            `}
+            </Script>
+            <Component {...pageProps} />
+        </>
+    );
 
 }
