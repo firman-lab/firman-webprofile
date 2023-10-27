9814b4c12c358e5db03d3a4f206d9330faabb56c
Firman Hidayat <fhidayat400@gmail.com>
2023-10-27 10:16:15 +0700

    update // file separator

--- utils/mdx.js
+++ utils/mdx.js
@@ -1,22 +1,22 @@
-import path from "path";
-import fs from "fs";
-import matter from "gray-matter";
-import readingTime from "reading-time";
-import { sync } from "glob";
+import path from 'path';
+import fs from 'fs';
+import matter from 'gray-matter';
+import readingTime from 'reading-time';
+import { sync } from 'glob';
 
-const articlesPath = path.join(process.cwd(), "articles");
+const articlesPath = path.join(process.cwd(), 'articles');
 
 // get unique article, when it is clicked on, by the user
 // on the blog page
 export async function getSlug() {
     const paths = sync(`${articlesPath}/*.mdx`);
 
-    return paths.map((path) => {
+    return paths.map((filePath) => {
         // holds the paths to the directory of the article
-        const parts = path.split("/"||"\\");
+        const parts = filePath.split(path.sep);
         const fileName = parts[parts.length - 1]; // gets the last part of path with /name.mdx
-        const [slug, _extension] = fileName.split(".");
-        console.log("slug", slug)
+        const [slug, _extension] = fileName.split('.');
+        console.log('slug', slug);
         return slug;
     });
 }
@@ -43,20 +43,20 @@ export async function getArticleFromSlug(slug) {
 
 // get the path that stores all the articles or blog post
 export async function getAllArticles() {
-    const articles = fs.readdirSync(path.join(process.cwd(), "/articles"));
+    const articles = fs.readdirSync(path.join(process.cwd(), '/articles'));
 
     return articles.reduce((allArticles, articleSlug) => {
         // get parsed data from mdx files in the "articles" dir
         const source = fs.readFileSync(
-            path.join(process.cwd(), "/articles", articleSlug),
-            "utf-8"
+            path.join(process.cwd(), '/articles', articleSlug),
+            'utf-8'
         );
         const { data } = matter(source);
 
         return [
             {
                 ...data,
-                slug: articleSlug.replace(".mdx", ""),
+                slug: articleSlug.replace('.mdx', ''),
                 readingTime: readingTime(source).text,
             },
             ...allArticles,
